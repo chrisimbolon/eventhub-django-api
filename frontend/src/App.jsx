f211775e02e2c36257d091da4623779a0e7669b9
Christyan Simbolon <sambungdata@gmail.com>
2026-02-05 17:59:28 +0700

    feat : update Route and Toster UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 // src/App.js
 
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Sidebar from "./components/layout/Sidebar";
-import Footer from "./components/layout/Footer";
-import Home from "./pages/Home";
-import EventsPage from "./pages/EventsPage";
-import EventDetailPage from "./pages/EventDetailPage";
-import AuthPage from "./pages/AuthPage";
-import AdminPage from "./pages/AdminPage";
-import CreateEvent from "./pages/CreateEvent";
 import { useAuth } from "./hooks/useAuth";
+import AdminPage from "./pages/AdminPage";
+import AuthPage from "./pages/AuthPage";
+import CreateEvent from "./pages/CreateEvent";
+import EventDetailPage from "./pages/EventDetailPage";
+import EventsPage from "./pages/EventsPage";
+import Home from "./pages/Home";
+
 
 export default function App() {
   const { user } = useAuth();
@@ -19,13 +20,17 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-slate-900">
       <Navbar />
+
+      <Toaster />
       <div className="flex">
         <Sidebar />
         <main className="flex-1 p-6">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<EventsPage />} />
-            <Route path="/events/:id" element={<EventDetailPage />} />
+            {/* <Route path="/events/:id" element={<EventDetailPage />} /> */}
+            <Route path="/events/:slug" element={<EventDetailPage />} />
+
             <Route
               path="/admin/*"
               element={user?.is_admin ? <AdminPage /> : <Navigate to="/" replace />}
