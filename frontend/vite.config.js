4f9fcf4931298d6dec247c6ae27b7c40aacdd355
Christyan Simbolon <sambungdata@gmail.com>
2026-02-08 02:16:39 +0700

    Fix: remove server on vite.confige

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react'
-import path from "path"
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,20 +10,9 @@ export default defineConfig({
     },
   },
 
-  server: {
-    host: true, // allow external access (0.0.0.0)
-    allowedHosts: [
-      'eventhub.chrisimbolon.dev',
-      'localhost',
-      '127.0.0.1'
-    ]
-  },
-
+  
   preview: {
-    allowedHosts: [
-      'eventhub.chrisimbolon.dev',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
-})
+    host: true,
+    allowedHosts: "all", 
+  },
+});
